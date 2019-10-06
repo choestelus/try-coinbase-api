7344fe91a7b4e0fb2d447d78c958f8358153b52f
Nattapong Amornbunchornvej <nattapong@satang.com>
2019-10-07 02:28:56 +0700

    [orderbook] init interface and type structure

---
+++ pkg/order/book.go
@@ -0,0 +1,33 @@
+package order
+
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+)
+
+// Order holds information of aggregated orderbook
+// From exchange, NumOrders denotes number of aggregated orders
+// and should not use for multiplying with Size
+type Order struct {
+	OrderID   string          `json:"order_id"`
+	Price     decimal.Decimal `json:"price"`
+	Size      decimal.Decimal `json:"size"`
+	NumOrders int64           `json:"num_orders"`
+}
+
+// Book holds general info of orderbook list
+// both bid side and ask side along with retrieval timestamp
+type Book struct {
+	Sequence  string    `json:"sequence"`
+	Bids      []Order   `json:"bids"`
+	Asks      []Order   `json:"asks"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// BookStreamer is main interface for using with
+// streaming API part, underlying that's synchronous
+// blocking API, should be wrapped with channel
+type BookStreamer interface {
+	Tick() <-chan Book
+}
