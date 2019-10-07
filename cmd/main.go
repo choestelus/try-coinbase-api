1251f87b210a7555a5a7935ee9098f8f9e11338e
Nattapong Amornbunchornvej <nattapong@chochoe.net>
2019-10-08 00:04:28 +0700

    [cmd] implement streaming report mode

--- cmd/main.go
+++ cmd/main.go
@@ -19,33 +19,58 @@ func main() {
 
 	switch cfg.Mode {
 	case "oneshot":
-		inputAsset := cfg.InputAsset
-		outputAsset := engine.PairOf(inputAsset)
-
-		orderbook := engine.OneShot(cfg.EngineConfig)
-		side := engine.PlaceSideToRetrieve(cfg.InputAsset)
-		orders, err := orderbook.GetOrdersBySide(side)
-		if err != nil {
-			logrus.Panic(err)
-		}
-		amount := decimal.RequireFromString(cfg.Amount)
-		consumed, matched := order.MatchUntilSatisfied(side, orders, amount)
-
-		Report(amount, consumed, matched, inputAsset, outputAsset)
+		ExchangeOneShot(cfg, engine)
 	case "service":
-		logrus.Panicf("not implemented: %v", cfg.Mode)
-		// bookstream := engine.OpenStream(cfg.EngineConfig)
-
+		ExchangeStream(cfg, engine)
 	default:
 		logrus.Warnf("unrecognized mode: %v", cfg.Mode)
 	}
+}
 
+// ExchangeOneShot groups exchanging operations for oneshot mode together
+func ExchangeOneShot(cfg config.Config, engine order.BookStreamer) {
+	inputAsset := cfg.InputAsset
+	outputAsset := engine.PairOf(inputAsset)
+
+	book := engine.OneShot(cfg.EngineConfig)
+	side := engine.PlaceSideToRetrieve(cfg.InputAsset)
+	orders, err := book.GetOrdersBySide(side)
+	if err != nil {
+		logrus.Panic(err)
+	}
+
+	amount := decimal.RequireFromString(cfg.Amount)
+	consumed, matched := order.MatchUntilSatisfied(side, orders, amount)
+
+	Report(book, amount, consumed, matched, inputAsset, outputAsset)
+}
+
+// ExchangeStream groups exchanging operations for service mode together
+func ExchangeStream(cfg config.Config, engine order.BookStreamer) {
+	stream := engine.OpenStream(cfg.EngineConfig)
+	for book := range stream {
+		inputAsset := cfg.InputAsset
+		outputAsset := engine.PairOf(inputAsset)
+
+		side := engine.PlaceSideToRetrieve(cfg.InputAsset)
+		orders, err := book.GetOrdersBySide(side)
+		if err != nil {
+			logrus.Panic(err)
+		}
+
+		amount := decimal.RequireFromString(cfg.Amount)
+		consumed, matched := order.MatchUntilSatisfied(side, orders, amount)
+
+		Report(book, amount, consumed, matched, inputAsset, outputAsset)
+	}
 }
 
 // Report pretty print summary of exchange conversion rate and transaction
-func Report(inputAmount, consumed, matched decimal.Decimal, inputAsset, outputAsset string) {
-	logrus.Infof("attempt to trading with [%v] %v", inputAmount.StringFixed(8), inputAsset)
-	logrus.Infof("consumed  [%v] %v", consumed.StringFixed(8), inputAsset)
-	logrus.Infof("got       [%v] %v", matched.StringFixed(8), outputAsset)
-	logrus.Infof("avg price [%v] %v/%v", consumed.Div(matched).StringFixed(8), inputAsset, outputAsset)
+func Report(book order.Book, inputAmount, consumed, matched decimal.Decimal, inputAsset, outputAsset string) {
+	logrus.Infof("---------------------%v---------------------------------------------", book.UpdatedAt.UTC())
+	logrus.Infof("attempt to trading with\t[%v] %v", inputAmount.StringFixed(8), inputAsset)
+	logrus.Infof("consumed               \t[%v] %v", consumed.StringFixed(8), inputAsset)
+	logrus.Infof("got                    \t[%v] %v", matched.StringFixed(8), outputAsset)
+	logrus.Infof("avg price              \t[%v] %v/%v", consumed.Div(matched).StringFixed(8), inputAsset, outputAsset)
+	logrus.Infof("---------------------------------------------------------------------------------------------------------")
 }
